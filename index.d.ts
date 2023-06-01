72ca61e62c868cd1a10bcb074fde7d7c3e874619
yakirn <yakirn@wix.com>
2023-06-01 18:09:35 +0300

    TS compile

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,1 @@
-export = WixEventually;
-
-type UnPromisify<T> = T extends Promise<infer U> ? U : T;
-
-declare function WixEventually<ReturnValue>(fn: () => ReturnValue, opts?: WixEventually.Opts): Promise<UnPromisify<ReturnValue>>;
-
-declare namespace WixEventually {
-  export interface Opts {
-    timeout?: number;
-    interval?: number;
-  }
-
-  function _with(overrides: Opts): <ReturnValue>(fn: () => ReturnValue, opts?: WixEventually.Opts) => Promise<UnPromisify<ReturnValue>>;
-  export { _with as with }
-}
+declare module 'trier-promise';
--- index.ts
+++ index.ts
@@ -1,27 +1,24 @@
 import trier from 'trier-promise';
 
 type UnPromisify<T> = T extends Promise<infer U> ? U : T;
-
 export interface Options {
   timeout?: number;
   interval?: number;
 }
-
-
-
 const defaults: Options = {
   timeout: 10000,
   interval: 200
 }
+type aFunction<R> = () => R
 
- function eventually<ReturnValue>(fn: () => ReturnValue, opts?: Options): Promise<UnPromisify<ReturnValue>> {
+function eventually<ReturnValue>(fn: aFunction<ReturnValue>, opts?: Options): Promise<UnPromisify<ReturnValue>> {
   return Promise.resolve().then(() => {
     let error = null
     const action = () => Promise.resolve().then(fn).catch(err => {
       error = err
       throw err
     })
-    const options = Object.assign({action}, defaults, opts)
+    const options = Object.assign({ action }, defaults, opts)
 
     return trier(options).catch(() => {
       if (error !== null) {
@@ -34,5 +31,5 @@ const defaults: Options = {
 
 export default {
   eventually,
-  with: overrides => (fn, opts) => eventually(fn, Object.assign({}, overrides, opts))
+  with: <R>(overrides: Options) => (fn: aFunction<R>, opts: Options) => eventually(fn, Object.assign({}, overrides, opts))
 }
